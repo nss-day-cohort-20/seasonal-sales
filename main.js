85349e2f03504629ee90beb8db11d7b8c7c5bdfa
Joe Shepherd <joeshepmedia@gmail.com>
2017-06-19 14:29:57 -0500

    create XHR to fetch categories data

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 console.log("Yup, it loaded");
 
-let reqProducts = new XMLHttpRequest();
-let reqCats = new XMLHttpRequest();
-
+// get products
 function displayProducts() {
   let prodArr = JSON.parse(event.target.responseText).products;
-
   console.log("products array", prodArr);
 }
 
-reqProducts.addEventListener("load", displayProducts)
+function displayCategories() {
+  let catArr = JSON.parse(event.target.responseText).categories;
+  console.log("category array", catArr);
+}
 
-reqProducts.open("GET", "data/products.json");
-reqProducts.send();
+function getCategories() {
+  let reqCategories = new XMLHttpRequest();
+  reqCategories.addEventListener("load", displayCategories)
+  reqCategories.open("GET", "data/categories.json");
+  reqCategories.send();
+}
+
+function getProducts() {
+  let reqProducts = new XMLHttpRequest();
+  reqProducts.addEventListener("load", displayProducts)
+  reqProducts.open("GET", "data/products.json");
+  reqProducts.send();
+}
+getProducts();
+getCategories();
