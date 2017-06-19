8f394fae1f235d424b36adbc7da9c12c38da3032
Joe Shepherd <joeshepmedia@gmail.com>
2017-06-19 13:45:13 -0500

    create XHR to fetch products

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log("Yup, it loaded");
+
+let reqProducts = new XMLHttpRequest();
+let reqCats = new XMLHttpRequest();
+
+function displayProducts() {
+  let prodArr = JSON.parse(event.target.responseText).products;
+
+  console.log("products array", prodArr);
+}
+
+reqProducts.addEventListener("load", displayProducts)
+
+reqProducts.open("GET", "data/products.json");
+reqProducts.send();
