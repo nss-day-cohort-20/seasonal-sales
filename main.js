afda0bcbd92da98a6715ead37b2ba538b1b7eca0
Joe Shepherd <joeshepmedia@gmail.com>
2017-06-20 11:08:19 -0500

    change prices when season is selected; refactor to calc discount when buidling DOM Obj

--- main.js
+++ main.js
@@ -9,15 +9,35 @@ function displayProducts(productArr) {
   let cardArr = productArr.map( function(product) {
     return buildCard(product)
   })
-  console.log("cardArr ready to go into DOM", cardArr);
+  // console.log("cardArr ready to go into DOM", cardArr);
   cardArr.forEach( function(card) {
     let cardWrapper = document.createElement("article");
     cardWrapper.innerHTML = card;
     productWrapper.appendChild(cardWrapper);
   });
 }
-// TODO function to calc discount
-// TODO function to change prices in DOM
+
+// TODO function to respond to dropdown selection
+document.getElementById("dropdown").addEventListener("change", function() {
+  let selectedSeason = event.target.value
+  console.log("selected Season", selectedSeason);
+  let seasonCategory = categories.filter( function(category) {
+    return category.season_discount.toLowerCase() === selectedSeason.toLowerCase();
+  });
+  let catId = seasonCategory[0].id
+  // grab all the product cards from the DOM
+  let prodCards = document.getElementsByClassName("prodCard")
+  for( let i = 0; i < prodCards.length; i++) {
+    if ( parseInt(prodCards[i].getAttribute("data-catId")) === catId) {
+      console.log("product card", prodCards[i]);
+      let pTags = prodCards[i].getElementsByTagName("p");
+      for ( let i = 0; i < pTags.length; i++) {
+        pTags[i].classList.toggle("isHidden");
+      }
+    }
+  }
+
+});
 
 function buildDOMObj() {
   // loop through products and categories to grab Prod name, Dept, Price, and cat ID
@@ -34,26 +54,36 @@ function buildDOMObj() {
       dept: categoryItem[0].name,
       name: currentProduct.name,
       price: currentProduct.price,
-      catId: currentProduct.category_id
+      catId: currentProduct.category_id,
+      discountedPrice: calculateDiscountPrice(currentProduct.price, categoryItem[0].discount)
     }
     return prodObj
   });
   // For now, just see if map worked
   console.log("prod arr", productArr);
-  displayProducts(productArr);
+  // TODO call a function to populate DOM
+  displayProducts(productArr)
 }
 
 function buildCard(prodObj) {
-  let card = `<div class="prodCard" data-catId="${prodObj.catId}">
+  let card = `<div class="prodCard" data-catId=${prodObj.catId}>
                 <h2>${prodObj.name}</h2>
                 <h3>${prodObj.dept}</h3>
                 <p>$${prodObj.price}</p>
+                <p class="isHidden">$${prodObj.discountedPrice}</p>
               </div>`;
   return card;
 }
+
 // Test buildCard:
 // let TempObj = {name: "Furby", dept: "Toys", price: 12.75}
 // console.log( "card", buildCard(TempObj) );
+
+function calculateDiscountPrice(origPrice, discount) {
+  return +(origPrice * (1.00 - discount)).toFixed(2)
+}
+// Test of calculation with dummy data
+console.log("discounted price", calculateDiscountPrice(3.99, .25));
 
 function setProducts() {
   products = JSON.parse(event.target.responseText).products;
